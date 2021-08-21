3bb2da7a242bf8629ae6f197569d5e287cbde4ce
chmez <chmez070@gmail.com>
2021-08-21 18:13:00 +0300

    Move settings of days to a separate file

--- index.php
+++ index.php
@@ -1,40 +1,8 @@
 <?php
 
-const DAYS = [
-  [
-    'name' => 'неділя',
-    'suffix' => 2,
-  ],
-  [
-    'name' => 'п0неділ0к',
-    'icon' => 46589, // The "UA Monday First Letters" icon identifier.
-    'delete' => 2,
-  ],
-  [
-    'name' => 'вівт0р0к',
-    'icon' => 46586, // The "UA Tuesday First Letter" icon identifier.
-    'delete' => 1,
-    'suffix' => 1,
-  ],
-  ['name' => 'середа'],
-  [
-    'name' => '4етвер',
-    'suffix' => 2,
-  ],
-  [
-    'name' => 'п\'ятниця',
-    'icon' => 46588, // The "UA Friday First Letter" icon identifier.
-    'delete' => 1,
-  ],
-  [
-    'name' => 'су60та',
-    'suffix' => 1,
-  ],
-];
-
 [$day_of_week, $month, $day_of_month] = explode(',', date('w,n,j'));
-
-$day = DAYS[$day_of_week];
+$days = json_decode(file_get_contents('days.json'), TRUE);
+$day = $days[$day_of_week];
 $name = $day['name'];
 
 if ($month === '8' && $day_of_month === '24') {
