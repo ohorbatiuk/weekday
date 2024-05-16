ebd4c7700dcf761701283e533769c245dbdf3b49
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-05-16 17:23:33 +0300

    Use tabs instead of spaces as indents

--- index.php
+++ index.php
@@ -46,33 +46,33 @@ foreach (json_decode(file_get_contents('holidays.json'), TRUE) as $date) {
 }
 
 if ($holiday) {
-  if (isset($day['delete'])) {
-    $name = mb_substr($name, $day['delete']);
-  }
+	if (isset($day['delete'])) {
+		$name = mb_substr($name, $day['delete']);
+	}
 
-  /**
-   * Add dot(s) after a word to move the word to an icon as closely as possible.
-   *
-   * @see https://help.lametric.com/support/discussions/topics/6000060555
-   */
-  if (isset($day['suffix'])) {
-    $name .= str_repeat('.', $day['suffix']);
-  }
+	/**
+	 * Add dot(s) after a word to move the word to an icon as closely as possible.
+	 *
+	 * @see https://help.lametric.com/support/discussions/topics/6000060555
+	 */
+	if (isset($day['suffix'])) {
+		$name .= str_repeat('.', $day['suffix']);
+	}
 
-  /**
-   * The "UA" icon identifier.
-   *
-   * @see https://developer.lametric.com/icons
-   *
-   * @var int
-   */
-  $icon = $day['icon'] ?? 46587;
+	/**
+	 * The "UA" icon identifier.
+	 *
+	 * @see https://developer.lametric.com/icons
+	 *
+	 * @var int
+	 */
+	$icon = $day['icon'] ?? 46587;
 }
 
 $frame = ['text' => $name];
 
 if (isset($icon)) {
-  $frame['icon'] = $icon;
+	$frame['icon'] = $icon;
 }
 
 $response = ['frames' => [$frame]];
