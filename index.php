292e37b20745649743fcba275fca936895003b8a
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-05-16 17:21:02 +0300

    Support floating holidays

--- index.php
+++ index.php
@@ -7,8 +7,41 @@ $name = $day['name'];
 $holiday = FALSE;
 
 foreach (json_decode(file_get_contents('holidays.json'), TRUE) as $date) {
-  if ($month == $date['month'] && $day_of_month == $date['day']) {
-    $holiday = TRUE;
+  if ($month == $date['month']) {
+		if (isset($date['day']) && $day_of_month == $date['day']) {
+			$holiday = TRUE;
+			break;
+		}
+		elseif (
+			isset($date['day-of-week']) &&
+			$day_of_week == $date['day-of-week']
+		) {
+			if (($expected_repeats = $date['repeats'] ?? 1) > 1) {
+				$found_repeats = 0;
+				$current_day_of_week = $day_of_week;
+				$current_day_of_month = $day_of_month;
+
+				while ($current_day_of_month--) {
+					if ($current_day_of_week == $day_of_week) {
+						$found_repeats++;
+					}
+
+					if ($current_day_of_week) {
+						$current_day_of_week--;
+					}
+					else {
+						$current_day_of_week = 6;
+					}
+				}
+
+				if ($found_repeats != $expected_repeats) {
+					continue;
+				}
+			}
+
+			$holiday = TRUE;
+			break;
+		}
   }
 }
 
