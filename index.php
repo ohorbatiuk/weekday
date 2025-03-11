723a75f649fae121a2c219db1a564aa5b558c626
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2025-03-11 18:50:36 +0200

    Simplify the structure of holidays' data by presenting the month numbers as keys of array elements and the list of days of these months as values ​​of the same array's elements

--- index.php
+++ index.php
@@ -1,78 +1,45 @@
 <?php
 
-[$day_of_week, $month, $day_of_month] = array_map('intval', explode(',', date('w,n,j')));
-$day = (require 'days.php')[$day_of_week];
-$name = $day['name'];
-$holiday = FALSE;
+[$weekday, $month, $monthday] = array_map('intval', explode(',', date('w,n,j')));
 
-foreach (require 'holidays.php' as $date) {
-	if ($month === $date['month']) {
-		if ($day_of_month === ($date['day'] ?? 0)) {
-			$holiday = TRUE;
-			break;
-		}
-		elseif ($day_of_week === ($date['day-of-week'] ?? 0)) {
-			if (($expected_repeats = $date['repeats'] ?? 1) > 1) {
-				$found_repeats = 0;
-				$current_day_of_week = $day_of_week;
-				$current_day_of_month = $day_of_month;
+foreach ((require 'holidays.php')[$month] ?? [] as $dates)
+	if ($holiday = in_array($monthday, $dates = (array) $dates)) break;
+	else {
+		$dates = array_column(array_filter($dates, 'is_array'), 1, 0);
 
-				while ($current_day_of_month--) {
-					if ($current_day_of_week == $day_of_week) {
-						$found_repeats++;
-					}
+		if (isset($dates[$weekday])) {
+			[$current_weekday, $current_monthday] = [$weekday, $monthday];
 
-					if ($current_day_of_week) {
-						$current_day_of_week--;
-					}
-					else {
-						$current_day_of_week = 6;
-					}
-				}
+			while ($current_monthday--) {
+				if ($current_weekday === $weekday) $dates[$weekday]--;
 
-				if ($found_repeats != $expected_repeats) {
-					continue;
-				}
+				$current_weekday = $current_weekday ? $current_weekday - 1 : 6;
 			}
 
-			$holiday = TRUE;
-			break;
+			if ($holiday = $dates[$weekday] === 0) break;
 		}
 	}
-}
 
-if ($holiday) {
-	if (isset($day['delete'])) {
-		$name = mb_substr($name, $day['delete']);
-	}
+$frame['text'] = ($day = (require 'days.php')[$weekday])['name'];
+
+if ($holiday ?? FALSE) {
+	if (isset($day['delete'])) $frame['text'] = mb_substr($frame['text'], $day['delete']);
 
 	/**
 	 * Add dot(s) after a word to move the word to an icon as closely as possible.
 	 *
 	 * @see https://help.lametric.com/support/discussions/topics/6000060555
 	 */
-	if (isset($day['suffix'])) {
-		$name .= str_repeat('.', $day['suffix']);
-	}
+	if (isset($day['suffix'])) $frame['text'] .= str_repeat('.', $day['suffix']);
 
 	/**
 	 * The "UA" icon identifier.
 	 *
 	 * @see https://developer.lametric.com/icons
-	 *
-	 * @var int $icon
 	 */
-	$icon = $day['icon'] ?? 46587;
+	$frame['icon'] = $day['icon'] ?? 46587;
 }
-
-$frame = ['text' => $name];
-
-if (isset($icon)) {
-	$frame['icon'] = $icon;
-}
-
-$response = ['frames' => [$frame]];
 
 header('Content-Type: application/json');
 
-echo json_encode($response);
+echo json_encode(['frames' => [$frame]]);
