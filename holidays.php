cf38599b3c59189143610a360ac0f1f939707352
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2026-07-28 12:21:33 +0300

    #7 Move the date back two weeks

--- holidays.php
+++ holidays.php
@@ -3,7 +3,7 @@
 return [
 	5 => [[4, 3]], // @see https://en.wikipedia.org/wiki/Vyshyvanka_Day
 	6 => 28,
-	7 => 28,
+	7 => 15,
 	8 => [23, 24],
 	10 => 1, // @see https://en.wikipedia.org/wiki/Defenders_Day_(Ukraine)
 	11 => 21,
